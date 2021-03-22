b422b344688fccf5c3c1eec4617aecd06284e283
HtetArkarKo <spotifyhakk88@gmail.com>
2021-03-22 22:14:38 +0630

    band name generator

---
+++ 100days_of_code/01.band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city = input("What is the name of city you grew up in ? \n")
+pet = input("What is your pet name ? \n")
+print(f"Your band name could be {city} {pet}")
