6f7014e3f0eef0a6b5bd303bb7a1941b7c9ca933
HtetArkarKo <spotifyhakk88@gmail.com>
2021-04-10 17:15:31 +0630

    100 days of code journey : day 20, old school snake game first part completed : day 21, old school snake game second part completed

---
+++ 100days_of_code/20_21.snake_game/main.py
@@ -0,0 +1,35 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+# setting up for window
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title("Welcome to Snake Game !!!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(fun=snake.up, key='Up')
+screen.onkey(fun=snake.down, key='Down')
+screen.onkey(fun=snake.left, key='Left')
+screen.onkey(fun=snake.right, key='Right')
+
+# moving snake
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+screen.exitonclick()
