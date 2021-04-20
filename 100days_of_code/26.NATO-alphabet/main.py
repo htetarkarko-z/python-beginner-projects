e90bc4cc0440f4d4c5f595172e107d92dc29d5e9
htetarkarko-z <spotifyhakk88@gmail.com>
2021-04-21 02:50:46 +0630

    100 days of code journey : day 30, update NATO alphabet with error handelling

--- 100days_of_code/26.NATO-alphabet/main.py
+++ 100days_of_code/26.NATO-alphabet/main.py
@@ -1,6 +1,13 @@
 import pandas
 data_frame = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dic = {row.letter: row.code for (index, row) in data_frame.iterrows()}
-user = input('Enter your word: ').upper()
-result_list = [nato_dic[letter] for letter in user]
-print(result_list)
+is_on = True
+while is_on:
+    user = input('Enter your word: ').upper()
+    try:
+        result_list = [nato_dic[letter] for letter in user]
+    except KeyError:
+        print('Sorry only letter in the alphabet')
+    else:
+        print(result_list)
+        is_on = False
