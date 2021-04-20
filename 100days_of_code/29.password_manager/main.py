a6aa62f86d212d9ea07a32ba2d78107b4cd06aff
htetarkarko-z <spotifyhakk88@gmail.com>
2021-04-21 04:04:13 +0630

    100 days of code journey : day 30, add search function and error handeling

--- 100days_of_code/29.password_manager/main.py
+++ 100days_of_code/29.password_manager/main.py
@@ -3,39 +3,74 @@ from tkinter import messagebox
 import random
 import string
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR --------------------------- #
+
+
 def generate():
     password_entry.delete(0, END)
     letter = string.ascii_letters
     symbol = string.punctuation
     number = string.digits
-    password_list = [random.choice(letter) for char in range(random.randint(8, 10))]
-    password_list += [random.choice(symbol) for char in range(random.randint(2, 4))]
-    password_list += [random.choice(number) for char in range(random.randint(2, 4))]
+    password_list = [random.choice(letter)
+                     for char in range(random.randint(8, 10))]
+    password_list += [random.choice(symbol)
+                      for char in range(random.randint(2, 4))]
+    password_list += [random.choice(number)
+                      for char in range(random.randint(2, 4))]
     random.shuffle(password_list)
     password = ''.join(password_list)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     web = web_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    data_dic = {web: {
+        'username': username,
+        'password': password,
+    }}
 
     if len(web) < 1 or len(password) < 1:
         messagebox.showerror(title='Oops', message='Please don\'t leave any \
         field empty')
     else:
-        confirmation = messagebox.askokcancel(title=web, message=f'These are'
-            f' the detailed entered: \nEmail: {username}\nPassword: {password}'
-                f' \nIs it ok to save? ')
+        try:
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                json.dump(data_dic, file, indent=4)
+        else:
+            data.update(data_dic)
+            with open('data.json', 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
+            web_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-        if confirmation:
-            with open('data.txt', 'a') as file:
-                file.write(f"{web_entry.get()} | {username_entry.get()} | \
-                    {password_entry.get()} \n ")
-                web_entry.delete(0, END)
-                password_entry.delete(0, END)
+
+# --------------------------- SEARCH PASSWORD ------------------------------ #
+def search():
+    web = web_entry.get()
+    try:
+        with open('data.json', 'r') as data_file:
+            data = json.load(data_file)
+            search_file = data[web]
+    except FileNotFoundError:
+        messagebox.showerror(title='Error', message='No Data File Found')
+    except KeyError:
+        messagebox.showerror(
+            title='Error', message=f'No details for the {web} exists')
+    else:
+        messagebox.showinfo(
+            title=web, message=f"Email: {search_file['username']} \nPassword:"
+            f" {search_file['password']}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
@@ -50,9 +85,13 @@ canvas.grid(row=0, column=1)
 # website
 web_label = Label(text='Website:')
 web_label.grid(row=1, column=0)
-web_entry = Entry(width=35)
-web_entry.grid(row=1, column=1, columnspan=2)
+web_entry = Entry(width=21)
+web_entry.grid(row=1, column=1)
 web_entry.focus()
+
+# search
+search_button = Button(text='Search', command=search, width=10)
+search_button.grid(row=1, column=2)
 
 # username_email
 username_label = Label(text='Email/Username:')
@@ -68,7 +107,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # generate pwd button
-generate_button = Button(text='Generate Password', command=generate)
+generate_button = Button(text='Generate \nPassword',
+                         command=generate, width=10)
 generate_button.grid(row=3, column=2)
 
 # add
