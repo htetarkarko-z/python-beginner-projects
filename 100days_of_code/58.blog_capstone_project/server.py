a4d320bf3f8db1756a77bc33ed33928259e52b44
htetarkarko-z <spotifyhakk88@gmail.com>
2021-05-12 20:15:41 +0630

    100 days of code journey : day 60, send email from contact page with POST method

--- 100days_of_code/58.blog_capstone_project/server.py
+++ 100days_of_code/58.blog_capstone_project/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 app = Flask(__name__)
 
 POST_API = "https://api.npoint.io/0067e63917ca7a5034d9"
 response = requests.get(url=POST_API)
 blog_posts = response.json()
-
+EMAIL = "spotifyhakk88@gmail.com"
+PWD = "00990988"
 
 @app.route("/")
 def index():
@@ -25,6 +27,22 @@ def contact():
 @app.route("/post/<post_id>")
 def post(post_id):
     return render_template("post.html", post_id=int(post_id), blog_posts=blog_posts)
+
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PWD)
+            connection.sendmail(
+                from_addr= EMAIL,
+                to_addrs= EMAIL,
+                msg= f"Subject: HELLO\n\n name: {data['name']}\nemail: {data['email']}\nPhone No: {data['phoneno']}\nMessage: {data['message']}\n"
+            )
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 if __name__ == "__main__":
