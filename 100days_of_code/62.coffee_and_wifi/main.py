9e99becca2bdfd50568c2edf4f41fab7f6a5f00b
htetarkarko-z <spotifyhakk88@gmail.com>
2021-05-14 20:22:02 +0630

    100 days of code journey : day 62, coffee and wifi project with flask app

---
+++ 100days_of_code/62.coffee_and_wifi/main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.core import SelectField
+from wtforms.validators import DataRequired, URL
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Cafe Location on Google Map (URL)', validators=[
+                           DataRequired(), URL()])
+    open_time = StringField("Opening Time e.g. 8AM",
+                            validators=[DataRequired()])
+    close_time = StringField("Closing Time e.g. 5:30PM",
+                             validators=[DataRequired()])
+    coffee_rating = SelectField("Coffee Rating", choices=[
+                                '☕', '☕☕', '☕☕☕', '☕☕☕☕', '☕☕☕☕☕'], validators=[DataRequired()])
+    wifi_rating = SelectField("Wifi Strength Rating", choices=[
+                              '💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪'], validators=[DataRequired()])
+    power = SelectField("Power Socket Availability", choices=[
+                        '🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌'],
+                        validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        with open(file="cafe-data.csv", mode='a') as data:
+            data.write(
+                f"\n {form.cafe.data}, {form.location.data},"
+                f"{form.open_time.data}, {form.close_time.data},"
+                f"{form.coffee_rating.data}, {form.wifi_rating.data},"
+                f"{form.power.data}")
+        return redirect(url_for('cafes'))
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
